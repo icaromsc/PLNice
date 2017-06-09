70e73bc343edf79f55ab4ecf2e9d676fe3bb754e
icaromsc <icaromscastro@gmail.com>
2017-06-08 22:11:38 -0300

    Pre processamento finalizado

---
+++ Document.py
@@ -0,0 +1,6 @@
+class Document(object):
+    def __init__(self,name,text):
+        self.doc_name=name
+        self.text=text
+    
+
--- pre_processing.py
+++ pre_processing.py
@@ -1,6 +1,10 @@
 from os import getcwd
+from collections import Counter
 
-mypath=(getcwd())
+mypath=(getcwd()+"/texts/")
+
+
+
 def getListFiles(mypath):
     from os import listdir
     from os.path import isfile, join
@@ -8,15 +12,77 @@ def getListFiles(mypath):
     #print onlyfiles
     return onlyfiles
 
+
 def openFiles(listFiles):
     texts=[]
     for i in listFiles:
-        file=open(mypath + "/" + i, "r")
+        file=open(mypath  + i, "r")
         texts.append(file.read())
     return texts
 
+
+def getStopWords():
+    file = open("/home/infobio/PycharmProjects/PLNice/english_stop_words.txt", "r")
+    words=file.read().split()
+    return words
+
+
+def removeStopWords(file,stopwords):
+    removed=[]
+    file = removeJunk(file)
+    file=file.split()
+    for f in file:
+        if f.lower() not in stopwords:
+            removed.append(f)
+    return removed
+
+def removeJunk(file):
+    file = file.replace(":", "")
+    file = file.replace(",", "")
+    file = file.replace(".", "")
+    file = file.replace("?", "")
+    file = file.replace(")", "")
+    file = file.replace("(", "")
+    file = file.replace(";", "")
+    return file
+
+
+
+#inicializacao
 print mypath
 texts=openFiles(getListFiles(mypath))
-print texts[0]
+#print texts[0]
+words=getStopWords()
+print "stopwords:\n"
+print words
+processeds=[]
+terms=set()
+freqTerms=[]
+
+#remove stopwords
+for text in texts:
+    print "texto:\n*\n*\n",text
+    processed=removeStopWords(text,words)
+    print "texto sem stopwords:\n*\n*\n",processed
+    freqTerms.append(Counter(processed))
+    terms.update(set(processed))
+    print "n palavras txt original:", len(text)
+    print "n palavras txt processado:", len(processed)
+    processeds.append(processeds)
+
+
+
+
+print "*\n*\ntexts processed",len(processeds)
+print "*\n*\nn terms:",len(terms)
+print "terms:\n*\n",terms
+print "freq terms:\n*\n",freqTerms[0]
+
+
+
+
+
+
+
 
 
